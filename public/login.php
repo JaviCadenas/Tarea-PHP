302da403c20dcba906e522441c13eb68edfa3546
JaviCadenas <javilusu78100@gmail.com>
2025-11-26 20:25:55 +0100

    TerminandoLOGIN

--- public/login.php
+++ public/login.php
@@ -1,47 +1,42 @@
 <?php
-// 1. Nos conectmos a la base de datos
-require '../app/pdo.php';
-
-// 2. Iniciamos la sesión en la base de datos
+require '../app/pdo.php'; // con esto nos conectamos a la base de datos
+require '../app/utils.php'; // Esto sirve para evitar XSS
 session_start();
 
-$mensaje = ""; // Aquí guardaremos el error si falla el login
+$error = '';
 
-// 3. con esto sabemos si el formulario fue enviado con POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $user_form = $_POST['username']; // Aqui se guarda lo que escribio en el input
-    $pass_form = $_POST['password']; // Aqui se guarda la contraseña que escribio
+// Si envían el formulario (POST) Aqui comprobamos si se ha enviado con el metodo POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-    // 4. Esta es la consulta para buscar al usuario en la base de datos
-    // Usamos :u para evitar inyecciones SQL
-    $sql = "SELECT * FROM usuarios WHERE username = :u";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([':u' => $user_form]);
-    
-    $usuario_bd = $stmt->fetch(); // la informacion encontrada en la consulta, lo guardamos
+    // Aqui buscamos el usuario en la base de datos
+    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE username = :u");
+    $stmt->execute([':u' => $username]);
+    $user = $stmt->fetch();
 
-    // 5. Verificamos la contraseña
-    // password_verify compara la constraseña escrita con el hash de la base de datos
-    if ($usuario_bd && password_verify($pass_form, $usuario_bd['password'])) {
-        
-        // Aqui guardamos quien es en la sesión
-        $_SESSION['user_id']  = $usuario_bd['id'];
-        $_SESSION['username'] = $usuario_bd['username'];
-        
-        // Lo mandamos a la página principal
-        header('Location: index.php');
+    // Verificamos la contraseña y la compara con el hash de la BD
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header("Location: index.php"); // Redirigir a la pagina principal
         exit;
     } else {
-        // Fallo
-        $mensaje = "Usuario o contraseña incorrectos";
+        $error = "Usuario o contraseña incorrectos";
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Acceso</title>
-</head>
+<head><title>Login</title></head>
 <body>
-    <h2>Iniciar Sesión</h2>
+    <h1>Entrar al Inventario</h1>
+    <?php if($error): ?><p style="color:red"><?= e($error) ?></p><?php endif; ?>
+    
+    <form method="POST">
+        Usuario: <input type="text" name="username" required><br>
+        Clave: <input type="password" name="password" required><br>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
