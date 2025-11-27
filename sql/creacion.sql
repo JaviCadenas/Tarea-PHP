84d7d8368f8dd0d9e90609fe582e0abe4299b3b8
Ignachio-cmd <nachoignacio.bazar@gmail.com>
2025-11-27 16:23:36 +0100

    CreacionBaseDeLaBaseDeDatos

---
+++ sql/creacion.sql
@@ -0,0 +1,32 @@
+-- sql/schema.sql
+CREATE DATABASE IF NOT EXISTS inventario_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE inventario_db;
+
+-- Tabla los usuarios para los Login
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Tabla de Items, basicamente el Inventario en sí
+CREATE TABLE IF NOT EXISTS items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    categoria VARCHAR(50),
+    ubicacion VARCHAR(50),
+    stock INT DEFAULT 0
+);
+
+-- Tabla para las auditorías
+CREATE TABLE IF NOT EXISTS auditoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    accion VARCHAR(255) NOT NULL,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Usuario Administrador por defecto
+-- Usuario: admin
+-- Contraseña: admin
+INSERT IGNORE INTO usuarios (username, password) VALUES 
+('admin', 'admin');
